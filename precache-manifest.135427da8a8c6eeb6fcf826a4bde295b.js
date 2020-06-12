37fd9694e54d85fedcf8f7a259332410e43d2548
Alvin Yang <alvin.yang73@gmail.com>
2020-06-12 15:26:45 -0700

    Updates

---
+++ precache-manifest.135427da8a8c6eeb6fcf826a4bde295b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74dac4b29f377cd0cbd91bea8bea306",
+    "url": "/miles-test/index.html"
+  },
+  {
+    "revision": "b968f455878ba1f558e7",
+    "url": "/miles-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c478643d6287c7da4548",
+    "url": "/miles-test/static/js/2.726c3dd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/miles-test/static/js/2.726c3dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b968f455878ba1f558e7",
+    "url": "/miles-test/static/js/main.7088a03d.chunk.js"
+  },
+  {
+    "revision": "0f3a9e0629a4ee607c3f",
+    "url": "/miles-test/static/js/runtime-main.8702be23.js"
+  }
+]);
